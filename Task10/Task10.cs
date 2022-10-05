504b3e3ceb6cb1495be1a64f1d88754a6a20fe8f
Alexey <aleksei_gudovich@mail.ru>
2022-10-05 23:42:29 +0300

    Task10

---
+++ Task10/Task10.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.WriteLine("Давай поработаем, ммм...");
+string username = Console.ReadLine();
+Console.WriteLine("Аа да, да - " + username);
+
+int n;
+Console.WriteLine("Введите трёхзначное число:");
+n = Convert.ToInt32(Console.ReadLine());
+
+if (n>=100)
+{
+ int second = (n%100)/10; 
+ Console.WriteLine(second+ " ");
+}
+else
+{
+ Console.WriteLine(username+", Вы ввели нетрёхзначное число");   
+}
